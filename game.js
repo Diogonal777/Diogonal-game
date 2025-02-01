7f733c194a43ecb5164f65aad2f6ac185f250ed0
Diogonal777 <voitovichvadim0@gmail.com>
2025-02-01 23:05:40 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,37 @@
-<!doctype html>
-<html lang="en"> 
- <head> 
-  <meta charset="UTF-8"> 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-  <title>My Phaser Game</title> 
-  <script src="https://cdn.jsdelivr.net/npm/phaser@3.55.2/dist/phaser.min.js"></script> 
-  <style>
-        body { margin: 0; }
-        canvas { display: block; background-color: #000; }
-    </style> 
- </head> 
- <body> 
-  <script src="game.js"></script> 
- </body>
-</html>
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('player', 'https://example.com/player.png'); // Замените на свой спрайт
+}
+
+function create() {
+    player = this.physics.add.image(400, 300, 'player');
+}
+
+function update() {
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.LEFT)) {
+        player.x -= 5;
+    } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.RIGHT)) {
+        player.x += 5;
+    }
+
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.UP)) {
+        player.y -= 5;
+    } else if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.DOWN)) {
+        player.y += 5;
+    }
+}
+
