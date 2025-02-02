2e9849bbc5f511d922ea90cd28d3fc2dbf096d42
Diogonal777 <voitovichvadim0@gmail.com>
2025-02-02 16:01:17 +0300

    Update game.js

--- game.js
+++ game.js
@@ -102,6 +102,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);  
         
         for (const meteor of meteors) {
             ctx.drawImage(meteorImage, meteor.x, meteor.y, meteor.width, meteor.height);
@@ -111,8 +113,6 @@ document.addEventListener("DOMContentLoaded", () => {
         ctx.font = "20px Arial";
         ctx.fillText(`Score: ${score}`, 10, 30);
         ctx.fillText(`High Score: ${highScore}`, 10, 60);
-
-        ctx.drawImage(shipImage, ship.x, ship.y, ship.width, ship.heigh); 
     }
 
     function showGameOver() {
